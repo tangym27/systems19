0cf37393a43652a1724801d7905a9267c1c33f3f
Michelle Tang <mtang1@stuy.edu>
2018-12-28 02:04:23 -0500

    fixed

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,38 @@
 #include "pipe_networking.h"
 
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    printf("\nEnding server\n");
+    exit(0);
+  }
+}
 
 int main() {
+  signal(SIGINT, sighandler);
 
   int to_client;
   int from_client;
+  char message[BUFFER_SIZE];
 
-  from_client = server_handshake( &to_client );
+  while(1) {
+    from_client = server_handshake( &to_client );
+
+    // continue reading until an error/end of file
+    while(read(from_client, message, BUFFER_SIZE) > 0) {
+      printf("[SERVER] Message server received: %s\n", message);
+
+      //process the data
+      for(int i = 0; i < strlen(message); i++) {
+        message[i] += 13;
+      }
+      message[strlen(message)-1]='!';
+
+      // write back to client
+      write(to_client, message, BUFFER_SIZE);
+      printf("[SERVER] Message server sent back: %s\n", message);
+    }
+
+    // set up for new client
+    printf("[SERVER] Client disconnected.\n");
+  }
 }
--- pipe_networking.c
+++ pipe_networking.c
@@ -1,19 +1,14 @@
 #include "pipe_networking.h"
 
-static void sighandler(int signo){
-  // Catches the SIGUSR1 signal
 
+static void sighandler(int signo){
 	if (signo == SIGINT){
-		//Before exiting, append a message to a file noting that the program exited due to SIGINT
-		printf("Exited process %d due to SIGINT \n", getpid());
+		printf("\nExited process %d due to SIGINT. Removed WKP.\n", getpid());
 		remove("wkp");
-	//	remove(client_pipe);
-		// char message[] =  "The program ended due to SIGINT\n";
-		// int w = write(fd, message, sizeof(message));
-		// close(fd);
 		exit(0);
 	}
 }
+
 /*=========================
   server_handshake
   args: int * to_client
@@ -22,40 +17,28 @@ static void sighandler(int signo){
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-		char message[1024];
-   signal(SIGINT, sighandler);
-
+  signal(SIGINT, sighandler);
   char buffer[HANDSHAKE_BUFFER_SIZE];
   if ( mkfifo("wkp", 0644) < 0 ) {
-    printf("Error %d: %s\n", errno, strerror(errno));
-    exit(0);
+    printf("[SERVER] Error %d: %s\n", errno, strerror(errno));
+    exit(1);
   }
-  printf("Congrats! The server pipe was just created.\n");
-	printf("Still waiting for a client/friend...\n" );
-
-	int from_client = open("wkp", O_RDONLY);
-  printf("Connecting to a client!\n");
-
+  printf("[SERVER] Congrats! The server pipe was just created.\n");
+  printf("[SERVER] Waiting for a connection...\n");
+  int from_client = open("wkp", O_RDONLY);
+  printf("[SERVER] Connecting to a client!\n");
   memset(buffer, 0, sizeof(buffer));
   read(from_client, buffer, sizeof(buffer));
-  printf("Connecting to pipe %s (from client!)\n", buffer);
-	remove("wkp");
-
+  printf("[SERVER] Connecting to pipe %s (from client!) \n", buffer);
 
   *to_client = open(buffer, O_WRONLY);
   strcpy(buffer, ACK);
   write(*to_client, buffer, sizeof(buffer));
   read(from_client, buffer, sizeof(buffer));
-  printf("Houston. We have a connection.\n");
-
-	while(1){
-		read(from_client, message, sizeof(message));
-		printf("Received message from the client %s", message);
-		message[0] -= 1;
-		printf("Sending back message to client %s", message);
-		write(*to_client, message, sizeof(message));
-	}
+  printf("[SERVER] Houston. We have a connection.\n");
+  remove("wkp");
   return from_client;
+
 }
 
 
@@ -67,40 +50,39 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-	signal(SIGINT, sighandler);
   char buffer[HANDSHAKE_BUFFER_SIZE];
   char client_pipe[256];
   sprintf(client_pipe,"%d",getpid());
   strcpy(buffer,client_pipe);
   if ( mkfifo(client_pipe, 0644) < 0 ) {
-    printf("Error %d: %s\n", errno, strerror(errno));
-    exit(0);
+    printf("[CLIENT] Error %d: %s\n", errno, strerror(errno));
+    exit(1);
   }
-  printf("Congrats! A pipe was created! Name: %s\n", client_pipe);
+  printf("[CLIENT] Congrats! A pipe was created! Name: %s\n", client_pipe);
 
   *to_server = open("wkp", O_WRONLY);
 
   write(*to_server, buffer, sizeof(buffer));
-  printf("A connection was just established with the server!\n");
+  if (*to_server < 0){
+    printf("[CLIENT] Error %d: %s\n", errno, strerror(errno));
+    if (errno == 9){
+      printf("[CLIENT] There is likely no server open.\n" );
+    }
+    remove(client_pipe);
+    exit(1);
+  }
+
+  printf("[CLIENT] A connection was just established with the server!\n");
+
 
   int from_server = open(client_pipe, O_RDONLY);
 
   read(from_server, buffer, sizeof(buffer));
-  printf("Server sent a message! (second handshake): %s\n", buffer);
+  printf("[CLIENT] Server sent a message: %s\n", buffer);
 
   remove(client_pipe);
   strcpy(buffer, ACK);
   write(*to_server, buffer, sizeof(buffer));
-
-	char message[1024];
-	while(1){
-		  printf("Enter data: ");
-		  fgets(message, 1024, stdin);
-		  printf("CLIENT: Sending message %s", message);
-		  write(*to_server, message, sizeof(message));
-		  read(from_server, message, 1024);
-		  printf("CLIENT: received message %s", message);
-	}
 
   return from_server;
 }
